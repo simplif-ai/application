dc418a48d03ceda08aed328325f572930ea97be8
Springo <kevinmxia@gmail.com>
2017-09-22 23:54:37 -0400

    Add python code template.
    Do this so that all python scripts will have similar headers and structures for easy readability.

---
+++ Templates/PYTHON_CODE_TEMPLATE.py
@@ -0,0 +1,29 @@
+"""
+AUTHORS: <Names>
+
+PURPOSE:
+    <Explanation on purpose of code>
+
+DEVELOPER NOTES:
+    <Important information that readers or users should know>
+"""
+
+# =============================================================================
+# Libraries and Global Variables
+# =============================================================================
+
+
+
+# =============================================================================
+
+
+class SampleClass:
+    """ Comment describing overall purpose of class """
+
+    def __init__(self):
+        """ Initialize field variables """
+        pass
+
+    def sample_function(self):
+        """ Comment describing overall purpose of function """
+        pass
