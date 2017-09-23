036b043c93ec0ee3240b307f43ce27a6a7b6286c
Kevin Xia <kevinmxia@gmail.com>
2017-09-23 16:07:26 -0400

    Add basic LexicalChain.py code layout
    Do this to add basic structuring to code before writing in content

---
+++ SummarizationModule/LexicalChain.py
@@ -0,0 +1,41 @@
+"""
+AUTHORS: Kevin Xia
+
+PURPOSE:
+    Class definitions for lexical chain tools.
+
+DEVELOPER NOTES:
+    None
+"""
+
+# =============================================================================
+# Libraries and Global Variables
+# =============================================================================
+
+import nltk
+
+# =============================================================================
+
+
+class LexWord:
+    """ Class representing individual words within chains """
+
+    def __init__(self):
+        """ Initialize field variables """
+        pass
+
+
+class LexChain:
+    """ Class representing chains of words within the same lexical context """
+
+    def __init__(self):
+        """ Initialize field variables """
+        pass
+
+
+class LexChainGroup:
+    """ Class representing a possible grouping of chains """
+
+    def __init__(self):
+        """ Initialize field variables """
+        pass
