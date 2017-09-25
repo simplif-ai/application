6c32d01fccbcbd41bd525564bde164f8edd2f7b7
Springo <kevinmxia@gmail.com>
2017-09-25 11:12:50 -0400

    Add full text as a field for summarizer.
    Do this so that summarizer has something to summarize.

--- SummarizationModule/Summarizer.py
+++ SummarizationModule/Summarizer.py
@@ -20,10 +20,13 @@ import nltk
 class Summarizer:
     """ Summarizer objects generated to summarize code """
 
-    def __init__(self):
+    def __init__(self, full_text):
         """ Initialize field variables """
-        pass
+        self.full_text = full_text
 
     def extract_nouns(self):
         """ Extracts and returns all nouns from a body of text """
         pass
+
+    def get_full_text(self):
+        return self.full_text
