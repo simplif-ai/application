80ed4146503e2d992fc879c166eb796bc8fac3c4
Kevin Xia <kevinmxia@gmail.com>
2017-09-25 20:15:51 -0400

    Added unit testing script for Summarizer.py
    Do this so that Summarizer.py will be well-tested.

---
+++ SummarizationModule/Testing/SummarizerTest.py
@@ -0,0 +1,27 @@
+"""
+AUTHORS: Kevin Xia
+
+PURPOSE:
+    Unit tests for Summarizer.py
+
+DEVELOPER NOTES:
+    None
+"""
+
+# =============================================================================
+# Libraries and Global Variables
+# =============================================================================
+
+import unittest
+from SummarizationModule.Summarizer import Summarizer
+
+# =============================================================================
+
+
+class TestNounExtraction(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    """ Run main if this python file is executed """
+    unittest.main()
