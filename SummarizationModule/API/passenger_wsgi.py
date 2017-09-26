3eb7439653acbc6c93974581187500d20886db05
Ian Renfro <ian@renfro.org>
2017-09-26 19:45:16 -0400

    API has the correct template
    The python template was added to the API

--- SummarizationModule/API/passenger_wsgi.py
+++ SummarizationModule/API/passenger_wsgi.py
@@ -1,10 +1,28 @@
+"""
+AUTHORS: Ian Renfro
+
+PURPOSE:
+    The codebase for the Summarizer API.
+
+DEVELOPER NOTES:
+    This is written in python with flask.
+"""
+
+# =============================================================================
+# Libraries and Global Variables
+# =============================================================================
+
 from flask import Flask, render_template, jsonify, request
+
+# Create a Flask object
 application = Flask(__name__)
 
-# Code Pertaining to the Summarizer API
+# =============================================================================
 
+# Create the route /summarizeTEST
 @application.route('/summarizeTEST')
 def summarizeTEST():
+        # Send a example of what the response would generally look like
 	text = [('this is', 1, 2), ('a sample', 3, 4), ('list', 5, 6), ('for testing', 7, 8), ('json', 9, 0)]
 	return jsonify({'text':text})
 
