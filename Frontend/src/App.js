3813ff85f6153b5ef60713501c9b417909cee4f2
avincent6 <vincent6@purdue.edu>
2017-10-05 23:48:08 -0400

    add cookie changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 import Routes from './Routes';
 import Nav from './pages/components/Nav';
 import Footer from './pages/components/Footer';
 import './App.css';
 
 const App = () => (
-  <BrowserRouter>
-    <div>
-      <Nav />
-      <Routes/>
-      <Footer />
-    </div>
-  </BrowserRouter>
+  <CookiesProvider>
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes/>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  </CookiesProvider>
 );
 
 export default App;
