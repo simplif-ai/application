ae3453933afa2e79d282740155f4c3b869a86c8c
avincent6 <vincent6@purdue.edu>
2017-10-06 01:34:05 -0400

    hide jwt login

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import { API_BASE_URL } from '../config';
 
 export default function apiFetch(endpoint, options = {}) {
-  options.headers = {
-    'x-access-token': 'token'
-  };
+  // 
+  // options.headers = {
+  //   'x-access-token': 'token'
+  // };
   return fetch(`${API_BASE_URL}/${endpoint}`,options);
 }
