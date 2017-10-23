f964b579b30b8c78865397da4ecf61e99abee557
Ian Renfro <ian@renfro.org>
2017-10-23 00:32:13 -0400

    Added more stuff to server script

--- SummarizationModule/remoteUpdate.sh
+++ SummarizationModule/remoteUpdate.sh
@@ -12,3 +12,13 @@ wget -O SummarizerNew.py https://raw.githubusercontent.com/simplif-ai/Applicatio
 echo "Downloading New Version of Passenger_WSGI.py"
 wget -O passenger_wsgiNew.py https://raw.githubusercontent.com/simplif-ai/Application/develop/SummarizationModule/API/passenger_wsgi.py -q --show-progress
 echo "Finished Downloading all files"
+echo "Diff'ing LexicalChain.py.  Output is in LexicalChainDiff.txt"
+echo -e "Local \t\t\t\t\t\t\t\t\t\t Server\n" > LexicalChainDiff.txt 
+diff -Bys LexicalChain.py LexicalChainNew.py >> LexicalChainDiff.txt
+echo "Diff'ing Summarizer.py.  Output is in SummarizerDiff.txt"
+echo -e "Local \t\t\t\t\t\t\t\t\t\t Server\n" > SummarizerDiff.txt 
+diff -Bys Summarizer.py SummarizerNew.py >> SummarizerDiff.txt
+echo "Diff'ing passenger_wsgi.py.  Output is in PassengerDiff.txt"
+echo -e "Local \t\t\t\t\t\t\t\t\t\t Server\n" > PassengerDiff.txt
+diff -Bys passenger_wsgi.py passenger_wsgiNew.py >> PassengerDiff.txt
+echo "Finished Diff'ing all files"
