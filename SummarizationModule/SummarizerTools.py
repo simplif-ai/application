46084869d3283476666fc57e9e4f6edf96077d03
Kevin Xia <kevinmxia@gmail.com>
2017-11-27 20:12:20 -0500

    Add SummarizerTools class.

---
+++ SummarizationModule/SummarizerTools.py
@@ -0,0 +1,26 @@
+"""
+AUTHORS: Kevin Xia
+
+PURPOSE:
+    Used for summarizing strange file formats.
+
+DEVELOPER NOTES:
+    None
+"""
+
+# =============================================================================
+# Libraries and Global Variables
+# =============================================================================
+
+import sys
+sys.path.append('../')
+
+# =============================================================================
+
+
+class SummarizerTools:
+    """ Summarizer Tool objects generated to deal with strange file formats """
+
+    def __init__(self):
+        """ Initialize field variables """
+    
